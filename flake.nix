2add1b9dc1a04bbe0eb59fe9200b8262c6b980c5
kczulko <karol.czulkowski@protonmail.com>
2024-09-02 23:14:12 +0200

    Fix flake name

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "canton-drivers nix flake for dev env and abci app mostly";
+  description = "rules_elm nix flake";
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
