a907124e1725097e10c67e1748baa396b8c7250f
kczulko <karol.czulkowski@protonmail.com>
2025-02-26 18:39:54 +0100

    feat: bazel 8.1.1

--- nix/overlays/bazel.nix
+++ nix/overlays/bazel.nix
@@ -19,7 +19,7 @@ let
 in
 {
   rulesElm = {
-    bazel8 = final.callPackage (bazelisk-bazel "8.1.0") { };
+    bazel8 = final.callPackage (bazelisk-bazel "8.1.1") { };
     bazel7 = final.callPackage (bazelisk-bazel "7.5.0") { };
   };
 }
