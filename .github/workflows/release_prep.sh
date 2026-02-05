0ecb10d96c0d3a501141c354de3c8b10c1b022c0
kczulko <karol.czulkowski@protonmail.com>
2026-02-05 16:25:31 +0100

    feat!: dropping WORKSPACE support

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -13,11 +13,9 @@ git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
 SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
 cat << EOF
-## Using bzlmod with Bazel 6 or later:
+## Using Bazel 7 or later:
 
-1. Add \`common --enable_bzlmod\` to \`.bazelrc\`.
-
-2. Add to your \`MODULE.bazel\` file:
+1. Add to your \`MODULE.bazel\` file:
 
 \`\`\`starlark
 bazel_dep(name = "rules_elm", version = "${TAG:1}")
@@ -33,39 +31,6 @@ elm.repository(
 use_repo(elm, "elm_package_elm_core")
 
 \`\`\`
-
-## Using WORKSPACE:
-
-\`\`\`starlark
-
-load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
-
-http_archive(
-    name = "rules_elm",
-    sha256 = "${SHA}",
-    strip_prefix = "${PREFIX}",
-    urls = "https://github.com/kczulko/rules_elm/releases/download/${TAG}/${ARCHIVE}",
-)
-
-load("@rules_elm//elm:dependencies.bzl", "elm_dependencies")
-elm_dependencies()
-load("@rules_java//java:rules_java_deps.bzl", "rules_java_dependencies")
-rules_java_dependencies()
-load("@com_google_protobuf//:protobuf_deps.bzl", "protobuf_deps")
-protobuf_deps()
-load("@rules_python//python:repositories.bzl", "py_repositories")
-py_repositories()
-load("@aspect_rules_js//js:repositories.bzl", "rules_js_dependencies")
-rules_js_dependencies()
-load("@rules_python//python:repositories.bzl", "py_repositories")
-py_repositories()
-
-load("@rules_shell//shell:repositories.bzl", "rules_shell_toolchains")
-rules_shell_toolchains()
-load("@rules_elm//elm:repositories.bzl", "elm_register_toolchains")
-elm_register_toolchains()
-load("@rules_elm_npm//:repositories.bzl", elm_npm_repositories = "npm_repositories")
-elm_npm_repositories()
 
 EOF
 
